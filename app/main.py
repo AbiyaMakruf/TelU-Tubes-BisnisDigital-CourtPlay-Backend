a156c93a582be4ea0a2b9a778bae528343d42707
AbiyaMakruf <aabbiiyyaa@gmail.com>
2025-10-16 23:03:42 +0700

    edit: refactoring total

--- app/main.py
+++ app/main.py
@@ -1,181 +1,51 @@
-import time
 import os
-import shutil
-import torch
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, UUID4
+import logging
+import threading
+from dotenv import load_dotenv
+from fastapi import FastAPI
 
-# Impor utilitas
-from .gcs_utils import download_model, download_original_video, upload_video, upload_thumbnail
-from .yolo_utils import inference_objectDetection, inference_playerKeyPoint
-from .supabase_utils import get_link_original_video, update_project_details, get_user_info, get_project_info, update_projects, insert_inference_log
-from .mailtrap_utils import send_success_email
-from .common_utils import get_video_duration, extract_first_frame_as_thumbnail, get_hardware_info
 
-app = FastAPI(title="YOLO Video Object Detection API")
+# Import from /utils
+from .utils.common_utils import download_if_model_not_exists, get_hardware_inference_info
 
-def check_and_download_model(bucket_name: str, model_type: str):
-    local_model_path = f"models/{model_type}/{model_type}.pt"
-    
-    if os.path.exists(local_model_path):
-        print(f"Model {model_type} sudah ada di {local_model_path}. Melewati pengunduhan.")
-        return
-    else:
-        print(f"Mengunduh model {model_type} dari GCS...")
-        download_model(bucket_name=bucket_name, model_type=model_type)
-        print(f"Model {model_type} berhasil diunduh.")
+# Import from /service
+from .service.inference_services import pull_messages
+
+# Konfigurasi GCS
+load_dotenv()
+PROJECT_ID = os.getenv("GCP_PROJECT_ID") 
+SUBSCRIPTION_ID = os.getenv("SUBSCRIPTION_ID")
+MAX_MESSAGES_PER_PULL = int(os.getenv("MAX_CONCURRENT_MESSAGES"))
+
+# Konfigurasi Log
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+# Konfigurasi Global
+bucket_name = "courtplay-storage"
+
+# Start FastAPI app
+app = FastAPI(title="Backend CourtPlay - Video Inference Worker")
 
 @app.on_event("startup")
 def startup_event():
     try:
-        print("Mengunduh model YOLO dari GCS...")
-
-        bucket = "courtplay-storage"
-        check_and_download_model(bucket, "objectDetection")
-        check_and_download_model(bucket, "playerKeyPoint")
-        check_and_download_model(bucket, "courtKeyPoint")
+        download_if_model_not_exists(bucket_name,"objectDetection")
+        download_if_model_not_exists(bucket_name,"playerKeyPoint")
+        download_if_model_not_exists(bucket_name,"courtKeyPoint")
 
     except Exception as e:
-        print(f"ERROR: Gagal memuat atau mengunduh model: {e}")
+        logger.error(f"Failed to download model. {e}")
 
-
-class InferenceRequest(BaseModel):
-    user_id: UUID4
-    project_id: UUID4
-    project_details_id: UUID4
-
-@app.post("/infer/")
-def infer_video(payload: InferenceRequest):
-    
-    # 1. SETUP: Variabel Statis dan Path
-    bucket_name = "courtplay-storage"
-    user_id = str(payload.user_id)
-    project_id = str(payload.project_id)
-    project_details_id = str(payload.project_details_id)
-    hw_info = get_hardware_info()
-    
-    local_project_root_dir = f"inference/{user_id}/{project_id}"
-    local_video_input_path = os.path.join(local_project_root_dir, "original_video.mp4")
-    
-    # Inisialisasi variabel untuk penggunaan finally block
-    link_thumbnail = None
-    
-    try:
-        # 2. Ambil Data Proyek dan User
-        username, email = get_user_info(user_id)
-        project_name, project_details_id_actual = get_project_info(project_id)
-        # Ambil link video asli dari Supabase
-        link_original_video = get_link_original_video(project_details_id)['link_video_original']
-        
-        # 3. Download dan Ekstraksi
-        download_original_video(bucket_name, user_id, project_id, link_original_video)
-        
-        path_thumbnail = extract_first_frame_as_thumbnail(local_video_input_path, os.path.join(local_project_root_dir, "thumbnails"))
-        link_thumbnail = upload_thumbnail(bucket_name, user_id, project_id, path_thumbnail)
-        
-        # 4. Inferensi dan Penghitungan Waktu
-        detection_time_start = time.time()
-        path_video_object_detection = inference_objectDetection(user_id, project_id)
-        detection_time_end = time.time()
-        
-        keypoint_time_start = time.time()
-        keypoint_result = inference_playerKeyPoint(user_id, project_id)
-        keypoint_time_end = time.time()
-        
-        # Ekstrak hasil
-        path_video_player_key_point = keypoint_result['path']
-        stroke_count = keypoint_result['counts']
-        
-        # Penghitungan akhir
-        duration_seconds = get_video_duration(local_video_input_path)
-        detection_inference_time = detection_time_end - detection_time_start
-        keypoint_inference_time = keypoint_time_end - keypoint_time_start
-        total_inference_time = keypoint_time_end - detection_time_start
-
-        # 5. Upload Video Hasil
-        link_video_object_detection = upload_video(bucket_name, "objectDetection", user_id, project_id, path_video_object_detection)
-        link_video_keypoints = upload_video(bucket_name, "playerKeyPoint", user_id, project_id, path_video_player_key_point)
-
-
-        # To Do
-        video_courtKeyPoint = "http://test"
-        image_heatmap_player = "http://test"
-
-        # 6. Update Supabase dan Notifikasi
-        if update_project_details(
-            project_details_id, 
-            link_video_object_detection, 
-            link_video_keypoints, 
-            link_thumbnail,
-            int(stroke_count['Forehand']), 
-            int(stroke_count['Backhand']), 
-            int(stroke_count['Serve']),
-            int(stroke_count['Ready_Position']), 
-            int(duration_seconds), 
-            int(total_inference_time),
-            video_courtKeyPoint,
-            image_heatmap_player
-        ):
-            send_success_email(
-                username=username,
-                project_name=project_name,
-                project_id=project_id,
-                receiver_email=email,
-                video_duration=duration_seconds
-            )
-            
-            update_projects(project_id, link_thumbnail, True)
-            
-            insert_inference_log(
-                project_id,
-                user_id,
-                int(detection_inference_time),
-                int(keypoint_inference_time),
-                int(total_inference_time),
-                hw_info['gpu_name'],
-                hw_info['vram_mb'],
-                hw_info['cpu_name'],
-                hw_info['cpu_threads'],
-                hw_info['ram_mb'],
-                hw_info['os_info'],
-                True
-            )
-
-        return {"status": "success", "message": "Inference and update complete."}
-        
-    except Exception as e:
-        # Menangkap error dan memberikan respon HTTP yang benar
-        print(f"FATAL ERROR during processing: {e}")
-        
-        insert_inference_log(
-                project_id,
-                user_id,
-                int(detection_inference_time),
-                int(keypoint_inference_time),
-                int(total_inference_time),
-                hw_info['gpu_name'],
-                hw_info['vram_mb'],
-                hw_info['cpu_name'],
-                hw_info['cpu_threads'],
-                hw_info['ram_mb'],
-                hw_info['os_info'],
-                False
-            )
-        
-        # Lakukan pembaruan status Supabase ke FAILED di sini (opsional tapi dianjurkan)
-        raise HTTPException(status_code=500, detail=f"Inference processing failed: {e}")
-
-    finally:
-        # 7. Pembersihan (Cleanup)
-        if os.path.exists(local_project_root_dir):
-            shutil.rmtree(local_project_root_dir)
+    listener_thread = threading.Thread(target=pull_messages(PROJECT_ID, SUBSCRIPTION_ID, MAX_MESSAGES_PER_PULL), daemon=True)
+    listener_thread.start()
 
 @app.get("/")
 def health_check():
-    hardware_info = get_hardware_info()
+    hardware_info = get_hardware_inference_info()
     
     return {
         "status": "ok", 
-        "message": "Object Detection Service Running",
+        "message": f"YOLO Inference Worker Running (PARALLEL Mode, Max Threads: {MAX_MESSAGES_PER_PULL})",
         "hardware": hardware_info
     }
---
+++ app/utils/yolo_utils.py
@@ -0,0 +1,106 @@
+import os
+import shutil
+import logging
+from ultralytics import YOLO
+
+from .common_utils import convert_avi_to_mp4
+
+# Konfigurasi Log
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+def inference_objectDetection(user_id, project_id):
+    project_path = f"inference/{user_id}/{project_id}"
+    temporary_path = "temporary_objectDetection"
+    video_original_path = f"{project_path}/original_video.mp4"
+    video_avi_format_path = os.path.join(project_path, temporary_path, "original_video.avi")
+    video_mp4_format_path = os.path.join(project_path, "objectDetection_video.mp4")
+
+    model = YOLO("models/objectDetection/objectDetection.pt")
+
+    try:
+        results = model(
+            source=video_original_path,
+            stream=True,
+            save=True,
+            project=project_path,
+            name=temporary_path,
+            exist_ok=True
+        )
+
+        for result in results:
+            result.save(filename="image.png")
+
+        convert_avi_to_mp4(video_avi_format_path, video_mp4_format_path)
+
+        logger.info("Success inference object detection")
+        return video_mp4_format_path
+    
+    except Exception as e:
+        logger.error("Error inference object detection")
+
+    finally:
+        delete_path = os.path.join(project_path,temporary_path)
+        if os.path.exists(delete_path):
+            shutil.rmtree(delete_path)
+
+def inference_playerKeyPoint(user_id, project_id):
+    project_path = f"inference/{user_id}/{project_id}"
+    temporary_path = "temporary_playerKeyPoint"
+    video_original_path = f"{project_path}/original_video.mp4"
+    video_avi_format_path = os.path.join(project_path, temporary_path, "original_video.avi")
+    video_mp4_format_path = os.path.join(project_path, "playerKeyPoint_video.mp4")
+
+    model = YOLO("models/playerKeyPoint/playerKeyPoint.pt")
+
+    stroke_counts = {
+        'Backhand': 0,
+        'Forehand': 0,
+        'Ready_Position': 0,
+        'Serve': 0
+    }
+
+    try:
+        results = model(
+            source=video_original_path,
+            stream=True,
+            save=True,
+            project=project_path,
+            name=temporary_path,
+            exist_ok=True
+        )
+
+        for result in results:
+            detected_class_ids = result.boxes.cls.tolist()
+
+            class_names = model.names
+
+            for class_id in detected_class_ids:
+                class_name = class_names[int(class_id)]
+                
+                if class_name in stroke_counts:
+                    stroke_counts[class_name] += 1
+
+            result.save(filename="image.png")
+
+        convert_avi_to_mp4(video_avi_format_path, video_mp4_format_path)
+
+        logger.info("Success inference player keypoint")
+
+        return {
+            'video_mp4_format_path':video_mp4_format_path,
+            'stroke_counts': stroke_counts
+        }
+    
+    except Exception as e:
+        logger.error("Error inference object detection")
+
+    finally:
+        delete_path = os.path.join(project_path,temporary_path)
+        if os.path.exists(delete_path):
+            shutil.rmtree(delete_path)
+
+
+# # To Do
+# - Heatmap player
+# - Ball drop heatmap
